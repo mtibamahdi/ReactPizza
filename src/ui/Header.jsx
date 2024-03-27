adbfe538c63436e918728cb5b4bf13262736986c
mtibamahdi <mtiba.mahdi@gmail.com>
2024-03-27 10:23:27 +0100

    Fixing Routes and Building App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">React Pizz Co.</Link>
+      <p>Mahdi</p>
+    </header>
+  );
+}
+
+export default Header;
