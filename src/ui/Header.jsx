739d40ef567474fe021ba490d2d894c1438213e5
mtibamahdi <mtiba.mahdi@gmail.com>
2024-03-28 09:10:22 +0100

    Fetching Orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header>
       <Link to="/">React Pizz Co.</Link>
+      <SearchOrder />
       <p>Mahdi</p>
     </header>
   );
